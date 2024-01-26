e02437a156eca0ad17bb7bcea94d6ecefdec4060
irene <irene.ndindaa@gmail.com>
2024-01-26 11:38:45 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+     def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+
+     def learn(self, string_input):
+        '''learns from a string and adds to self.knowledge.'''
+        self.knowledge.append(string_input)
+        
+        
